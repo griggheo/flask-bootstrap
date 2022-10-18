bf3106c2a72c54696ff57eb5afbb86db3bdee3d3
griggheo <grig@gheorghiu.net>
2022-10-17 18:35:06 -0700

    make script executable

--- .github/scripts/build-app.sh
+++ .github/scripts/build-app.sh
@@ -9,7 +9,6 @@ export PATH="$VIRTUAL_ENV/bin:$PATH"
 
 # this will use the pip in the venv
 pip3 install -r requirements.txt
-pip3 install -e .
 
 # remove pip because it has a disputed "will not fix" CVE: CVE-2018-20225
 #rm -rf $VIRTUAL_ENV/lib/python3.10/site-packages/pip*
