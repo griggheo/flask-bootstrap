8e3dd50a5326d8270078b33686c93dfbc4ad7412
griggheo <grig@gheorghiu.net>
2022-10-23 15:57:28 -0700

    generate cyclonedx sbom

--- .github/scripts/build-app.sh
+++ .github/scripts/build-app.sh
@@ -10,5 +10,9 @@ export PATH="$VIRTUAL_ENV/bin:$PATH"
 # this will use the pip in the venv
 pip3 install -r requirements.txt
 
+# generate cyclonedx SBOM
+pip3 install cyclonedx-bom
+cyclonedx-py -e -o cyclonedx-sbom.json
+
 # remove pip because it has a disputed "will not fix" CVE: CVE-2018-20225
 #rm -rf $VIRTUAL_ENV/lib/python3.10/site-packages/pip*
